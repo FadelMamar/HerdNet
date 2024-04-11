ebf68c246762990de0216a66633f3a4046a095dd
fadel <fadel.seydou@gmail.com>
2024-04-11 12:21:12 +0200

    commenting lines

--- run-ml-backend.bat
+++ run-ml-backend.bat
@@ -2,6 +2,9 @@
 call "C:\Users\Machine Learning\anaconda3\Scripts\activate.bat" "C:\Users\Machine Learning\anaconda3"
 call conda activate label-backend
 
+:: call set LABEL_STUDIO_LOCAL_FILES_SERVING_ENABLED=True
+:: call set LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT="D:\"
+
 label-studio-ml start HerdNet/my_ml_backend -p 9090
 
 :: deactivate
