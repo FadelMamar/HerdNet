424dfc40e4ad10133940b926f0af56fa9280b270
fadel <fadel.seydou@gmail.com>
2024-04-24 10:26:39 +0200

    correcting mistake

--- run-ml-backend.bat
+++ run-ml-backend.bat
@@ -5,7 +5,7 @@ call conda activate label-backend
 :: call set LABEL_STUDIO_LOCAL_FILES_SERVING_ENABLED=True
 :: call set LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT="D:\"
 
-label-studio-ml start HerdNet/my_ml_backend -p 9090
+label-studio-ml start ./my_ml_backend -p 9090
 
 :: deactivate
 call "C:\Users\Machine Learning\Desktop\workspace-wildAI\envs\label-studio\Scripts\deactivate.bat"
