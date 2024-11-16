fe57f34993dffd962a8fd2f8bffecd6f1881e082
fadel <fadel.seydou@gmail.com>
2024-11-16 13:32:26 +0000

    unneeded

--- annotation-data-tiling.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
-conda activate herdnet
-
-# tiling data
-python /home/ubuntu/workspace/HerdNet/tools/patcher.py \
-       /home/ubuntu/workspace/EBP-Lindanda-cam0 2048 2048 128 \
-       /home/ubuntu/workspace/EBP-Lindanda-cam0-splits -min 0.0 -all True
--- train_yolo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# logger
-wandb online
-
-# start training
-yolo detect train cfg=./yolo_cfg.yaml
-
